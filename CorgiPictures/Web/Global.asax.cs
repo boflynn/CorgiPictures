de04044c5c391f0c8780e7939d6823a95d797a97
Bo Flynn <boflynn@gmail.com>
2015-02-27 18:00:52 -0500

    Changed Code Analysis ruleset to all for all projects. Removed Azure Storage references from Web, only needed in the ImportJob.

--- CorgiPictures/Web/Global.asax.cs
+++ CorgiPictures/Web/Global.asax.cs
@@ -1,12 +1,9 @@
-﻿using System.Configuration;
-using System.Data.Entity;
-using System.Diagnostics;
+﻿using System.Data.Entity;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+
 using CorgiPictures.Model;
-using Microsoft.WindowsAzure.Storage;
-using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace CorgiPictures.Web
 {
@@ -19,30 +16,7 @@ namespace CorgiPictures.Web
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
-            InitializeStorage();
-
             Database.SetInitializer(new CreateDatabaseIfNotExists<CorgiPicturesContext>());
-        }
-
-        private void InitializeStorage()
-        {
-            // Open storage account using credentials from .cscfg file.
-            var storageAccount = CloudStorageAccount.Parse(ConfigurationManager.ConnectionStrings["CorgiPicturesStorage"].ToString());
-
-            Trace.TraceInformation("Creating images blob container");
-            var blobClient = storageAccount.CreateCloudBlobClient();
-            var imagesBlobContainer = blobClient.GetContainerReference("images");
-            if (imagesBlobContainer.CreateIfNotExists())
-            {
-                // Enable public access on the newly created "images" container.
-                imagesBlobContainer.SetPermissions(
-                    new BlobContainerPermissions
-                    {
-                        PublicAccess = BlobContainerPublicAccessType.Blob
-                    });
-            }
-
-            Trace.TraceInformation("Storage initialized");
         }
     }
 }
